93318429fb41f45ecc4a879410f254ecd9b1031d
Lea Dalibard <leadalibard@gmail.com>
2020-08-25 10:31:42 +0200

    06 Fizzbuzz completed with prompt, if else and condition for number to be between 0 and 100

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -9,8 +9,22 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
+(function () {
+    let number = prompt("Please enter a number between 1 and 100, and check your console");
+    var rem3 = number % 3
+    var rem5 = number % 5
+    if (0 <= number && number <= 100) {
+        if (rem3 === 0 && rem5 === 0) {
+            console.log(fizzbuzz);
+        } else if (rem3 === 0) {
+            console.log(buzz);
+        } else if (rem5 === 0) {
+            console.log(fizz);
+        } else {
+            console.log(number);
+        }
+    }
+    else {alert ('Please refresh and enter a number between 0 and 100')
+    }
 
 })();
