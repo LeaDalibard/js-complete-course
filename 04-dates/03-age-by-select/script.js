fabacf374840f8a168b8d407a5a71b4304d0f4bf
Lea Dalibard <leadalibard@gmail.com>
2020-08-25 13:59:58 +0200

    03 age by select completed

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,21 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function () {
+        var day = +document.getElementById("dob-day").value;
+        var monthbd = +document.getElementById("dob-month").value;
+        var year = +document.getElementById("dob-year").value;
+        var today = new Date();
+        var age = today.getFullYear()- year;
+        var month = today.getMonth()-monthbd;
+        if (month < 0 || (month === 0 && today.getDate() < day)) {
+            age = age - 1;
+        }
+        alert( 'You are '+age+' years old.' );
+
+    });
 
 })();
