f235f97aae2ee2fab330b6ebc6b822563970896d
Lea Dalibard <leadalibard@gmail.com>
2020-08-25 14:34:02 +0200

    04 locale date completed CAREFUL with month numbers + dont forget break for switch case

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -9,10 +9,81 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    var d = new Date();
+    var dy =d.getDay();
+    switch (dy) {
+        case 1:
+            var day = 'monday';
+            break;
+        case 2:
+            var day = 'tuesday';
+            break;
+        case 3:
+            var day = 'wednesday';
+            break;
+        case 4:
+            var day = 'thursday';
+            break;
+        case 5:
+            var day = 'friday';
+            break;
+        case 6:
+            var day = 'saturday';
+            break;
+        case 7:
+            var day = 'sunday';
+            break;
+    }
+    switch (d.getMonth()) {
+        case 0:
+            var month = 'january';
+            break;
+        case 1:
+            var month = 'february';
+            break;
+        case 2:
+            var month = 'mart';
+            break;
+        case 3:
+            var month = 'april';
+            break;
+        case 4:
+            var month = 'may';
+            break;
+        case 5:
+            var month = 'june';
+            break;
+        case 6:
+            var month = 'july';
+            break;
+        case 7:
+            var month = 'august';
+            break;
+        case 8:
+            var month = 'september';
+            break;
+        case 9:
+            var month = 'october';
+            break;
+        case 10:
+            var month = 'november';
+            break;
+        case 11:
+            var month = 'december';
+            break;
+    }
+    var year=d.getFullYear();
+    var date=d.getDate();
+    var h=d.getHours();
+    var m=d.getMinutes();
 
-    // your code here
+    document.getElementById("target").innerHTML = 'Today is '+ day+' '+ date +' '+month +' '+ year + '. It is '+ h +'h'+ m +'.';
+
 
 })();
+
+
+
