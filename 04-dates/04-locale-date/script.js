dd16f7bec51201b3677d5c3a1d006f5b24db118d
Lea Dalibard <leadalibard@gmail.com>
2020-08-26 15:57:39 +0200

    Changing switch method for months and day to array method

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -12,78 +12,110 @@
 (function () {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-    var d = new Date();
-    var dy =d.getDay();
-    switch (dy) {
-        case 1:
-            var day = 'monday';
-            break;
-        case 2:
-            var day = 'tuesday';
-            break;
-        case 3:
-            var day = 'wednesday';
-            break;
-        case 4:
-            var day = 'thursday';
-            break;
-        case 5:
-            var day = 'friday';
-            break;
-        case 6:
-            var day = 'saturday';
-            break;
-        case 7:
-            var day = 'sunday';
-            break;
-    }
-    switch (d.getMonth()) {
-        case 0:
-            var month = 'january';
-            break;
-        case 1:
-            var month = 'february';
-            break;
-        case 2:
-            var month = 'march';
-            break;
-        case 3:
-            var month = 'april';
-            break;
-        case 4:
-            var month = 'may';
-            break;
-        case 5:
-            var month = 'june';
-            break;
-        case 6:
-            var month = 'july';
-            break;
-        case 7:
-            var month = 'august';
-            break;
-        case 8:
-            var month = 'september';
-            break;
-        case 9:
-            var month = 'october';
-            break;
-        case 10:
-            var month = 'november';
-            break;
-        case 11:
-            var month = 'december';
-            break;
-    }
-    var year=d.getFullYear();
-    var date=d.getDate();
-    var h=d.getHours();
-    var m=d.getMinutes();
 
-    document.getElementById("target").innerHTML = 'Today is '+ day+' '+ date +' '+month +' '+ year + '. It is '+ h +'h'+ m +'.';
+    var d = new Date();
+    var year = d.getFullYear();
+    var date = d.getDate();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+    const monthIndex = d.getMonth()
+    const monthName = months[monthIndex]
+    const days = [
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday',
+        'sunday'
+    ]
+    const dayIndex = d.getDay()
+    const dayName = days[dayIndex]
+
+    document.getElementById("target").innerHTML = 'Today is ' + dayName + ' ' + date + ' ' + monthName + ' ' + year + '. It is ' + h + 'h' + m + '.';
 
 
 })();
+
+//OLD SOLUTION
+/*var d = new Date();
+var dy =d.getDay();
+switch (dy) {
+    case 1:
+        var day = 'monday';
+        break;
+    case 2:
+        var day = 'tuesday';
+        break;
+    case 3:
+        var day = 'wednesday';
+        break;
+    case 4:
+        var day = 'thursday';
+        break;
+    case 5:
+        var day = 'friday';
+        break;
+    case 6:
+        var day = 'saturday';
+        break;
+    case 7:
+        var day = 'sunday';
+        break;
+}
+
+switch (d.getMonth()) {
+    case 0:
+        var month = 'january';
+        break;
+    case 1:
+        var month = 'february';
+        break;
+    case 2:
+        var month = 'march';
+        break;
+    case 3:
+        var month = 'april';
+        break;
+    case 4:
+        var month = 'may';
+        break;
+    case 5:
+        var month = 'june';
+        break;
+    case 6:
+        var month = 'july';
+        break;
+    case 7:
+        var month = 'august';
+        break;
+    case 8:
+        var month = 'september';
+        break;
+    case 9:
+        var month = 'october';
+        break;
+    case 10:
+        var month = 'november';
+        break;
+    case 11:
+        var month = 'december';
+        break;
+}*/
 
 
 
