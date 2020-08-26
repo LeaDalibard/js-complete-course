0cc0ab27462f5cbc21ef0f17bb54842075cc5921
Lea Dalibard <leadalibard@gmail.com>
2020-08-26 09:27:42 +0200

    Walk one completed with loop for and console log

--- 05-arrays/03-walk-one/script.js
+++ 05-arrays/03-walk-one/script.js
@@ -9,7 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     var fruits = [
         "apple",
@@ -26,6 +26,13 @@
         "cherry",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+
+            for (let i = 0; i < 12; i++)
+            {
+                console.log(fruits[i]);
+            }
+        }
+    );
 
 })();
