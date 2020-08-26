2b0269db9edce2bba1fe46bb2fed8e678a7631e9
Lea Dalibard <leadalibard@gmail.com>
2020-08-26 15:40:38 +0200

    saving some lines in 02 change bcg 2

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -8,20 +8,13 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-function changeBackground(color) {
-    document.body.style.background = color;
-}
 
 (function() {
 
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
-        var colbg = document.getElementById("color").value;
-        changeBackground(colbg);
-
-
-
+        document.body.style.background = document.getElementById("color").value;
     });
 
 })();
