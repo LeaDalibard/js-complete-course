8cdc4cf9ad9fe03bec1a4379f6dc249511d75768
Lea Dalibard <leadalibard@gmail.com>
2020-08-28 15:34:03 +0200

    Ex 11 change-event1 completed with setAttribute maxlength +  onchange function,innerhtml => value.length

--- 06-dom/11-change-event-input-one/script.js
+++ 06-dom/11-change-event-input-one/script.js
@@ -9,8 +9,12 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
+(function () {
+    document.getElementById("pass-one").setAttribute("maxlength", "10");
+    let count = 0;
+    document.getElementById("pass-one").onchange = function () {
+        var x = document.getElementById("pass-one").value.length;
+        counter.innerHTML= x + "/10"
+    };
 
 })();
