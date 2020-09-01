d3e11efc739f9410e293b28cc0b8bc56c379c512
Lea Dalibard <leadalibard@gmail.com>
2020-09-01 17:11:14 +0200

    Updates before check out

--- 07-misc/02-typewriter-effect/script.js
+++ 07-misc/02-typewriter-effect/script.js
@@ -10,7 +10,27 @@
 // You will have time to focus on it later.
 
 (function() {
+    var txt = document.getElementById('target').textContent;
 
-    // your code here
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("target").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, (50));
+        }
+    }
+
+    typeWriter();
 
 })();
+/*   function typeWriter() {
+    for (var i = 0; i < txt.length; i++) {
+        setTimeout(function () {
+            document.getElementById("target").innerHTML += txt.charAt(i);
+        }, 3000);
+    }
+}
+typeWriter();
+
+var speed = 50*/
