c3335bc178ee6a5d6252ba34df4ff3c25ac45846
Lea Dalibard <leadalibard@gmail.com>
2020-09-01 14:41:51 +0200

    01 waving-text completed with loop and .font size, working but not super effective code/need to think of a betterway

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -9,8 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
+(function () {
+    let wawe=[];
+    var font=1;
+    var str = Array.from("Wouhouuuuu waaaawe");
+    for (let i = 0; i <= 5; i++) {
+        var y =str[i].fontsize(font);
+        font++;
+        wawe.push(y);
+    }
+    for (let i = 6; i <= 11; i++) {
+        var y =str[i].fontsize(font);
+        font--;
+        wawe.push(y);
+    }
+    for (let i = 12; i <= 17; i++) {
+        var y =str[i].fontsize(font);
+        font--;
+        wawe.push(y);
+    }
+    document.getElementById("target").innerHTML = wawe.join('');
 
 })();
