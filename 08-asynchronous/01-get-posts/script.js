3b964f5a8b198c152aad77deea15dc1133e1e400
Lea Dalibard <leadalibard@gmail.com>
2020-09-07 16:04:11 +0200

    07 ex 1

--- 08-asynchronous/01-get-posts/script.js
+++ 08-asynchronous/01-get-posts/script.js
@@ -8,17 +8,21 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
 (() => {
-   setTimeout(function (){
-       console.log(articles)
-   }, 5000);}
-)();
 
-let articles = [];
-document.getElementById("run").addEventListener("click", () => {
-    if (error) {
-       throw error;
+    let run = document.getElementById("run");
+
+    run.addEventListener("click", getPosts)
+
+    function getPosts() {
+        window.lib.getPosts(
+            function (error, posts) {
+                if (error) {
+                    console.log(1);
+                } else {
+                    console.log(null);
+                    console.log(posts);
+                }
+            })
     }
-    articles.push(window.lib.getPosts());
-});
+})();
