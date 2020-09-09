b800674bc2fe5afb2e07e896c733ab0648dbaa60
Lea Dalibard <leadalibard@gmail.com>
2020-09-09 14:15:12 +0200

    09 ex 05 redone using forEach

--- 09-fetch/05-delete/script.js
+++ 09-fetch/05-delete/script.js
@@ -17,12 +17,24 @@
         fetch("http://localhost:3000/heroes")
             .then(response => response.json())
             .then(function (heroes) {
+                heroes.forEach((heroe,index)=>{
+                    if (heroe.id==heroId){heroes.splice(index,1)}
+                })
+                console.log(heroes)
+            })
+    }
+
+})();
+
+/*function deleteHeroe(){
+        var heroId=document.getElementById("hero-id").value
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then(function (heroes) {
                 for (i=0; i<heroes.length; i++){
                     if (heroes[i].id==heroId){heroes.splice(i,1)}
                 }
                 console.log(heroes)
             })
-
     }
-
-})();
+*/
